1309909baef79eafd8dfbb58861febe44f0f2c4f
Kaveesha Weerakoon <94215796+Kaveesha-Weerakoon@users.noreply.github.com>
2023-11-10 12:47:31 +0530

    Update config.php

--- app/config/config.php
+++ app/config/config.php
@@ -1,15 +1,1 @@
-<?php
-  // DB Params
-  define('DB_HOST', 'localhost');
-  define('DB_USER', 'root');
-  define('DB_PASS', '');
-  define('DB_NAME', 'eco_plus');
 
-  // App Root
-  define('APPROOT', dirname(dirname(__FILE__)));
-  // URL Root
-  define('URLROOT', 'http://localhost/ecoplus');
-  define('IMGROOT', 'http://localhost/ecoplus/public/img');
-  define('JSROOT', 'http://localhost/ecoplus/public/js');
-  // Site Name
-  define('SITENAME', 'Eco Plus');

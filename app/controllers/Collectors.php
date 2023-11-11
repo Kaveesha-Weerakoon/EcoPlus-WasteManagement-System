450e5fe7bb0cb48ba41d88c7528062a8a19e1476
Nimalsh <nimalshapg@gmail.com>
2023-11-11 22:00:50 +0530

    collector_assistants_update

--- app/controllers/Collectors.php
+++ app/controllers/Collectors.php
@@ -237,6 +237,109 @@
      
     }
     
+
+    public function collector_assistants_update($assisId){
+   
+      if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+            $data = [
+               'assistant_id' => $assisId,
+               'name' =>trim($_POST['name']),
+               'nic' => trim($_POST['nic']),
+               'dob'=>trim($_POST['dob']),
+               'contact_no'=>trim($_POST['contact_no']),
+               'address' =>trim($_POST['address']),
+               'completed'=>'',
+  
+               'name_err' => '',
+               'nic_err' => '',
+               'dob_err'=>'',
+               'contact_no_err'=>'',
+               'address_err' =>'',
+      
+               
+        ];
+  
+        //validate name
+        if(empty($data['name'])){
+          $data['name_err'] = 'Please enter name';
+        } elseif (strlen($data['name']) > 255) {
+          $data['name_err'] = 'Name is too long';
+        }
+  
+        //validate NIC
+        if(empty($data['nic'])){
+          $data['nic_err'] = 'Please enter NIC';
+        }elseif(!(is_numeric($data['nic']) && (strlen($data['nic']) == 12)) && !preg_match('/^[0-9]{9}[vV]$/', $data['nic'])){
+          $data['nic_err'] = 'Please enter a valid NIC';
+        }elseif($this->collector_assistantModel->getCollectorAssisByNIC($data['nic'])){
+          $data['nic_err'] = 'NIC already exists';
+        }
+  
+        //validate DOB
+        if(empty($data['dob'])){
+          $data['dob_err'] = 'Please enter dob';
+        }
+  
+        // Validate Contact no
+        if(empty($data['contact_no'])){
+          $data['contact_no_err'] = 'Please enter contact no';
+        }elseif (!preg_match('/^[0-9]{10}$/', $data['contact_no'])) {
+          $data['contact_no_err'] = 'Please enter a valid contact number';
+        }
+  
+        // Validate Address
+        if(empty($data['address'])){
+          $data['address_err'] = 'Please enter adress';
+        } elseif (strlen($data['address']) > 500) {
+          $data['address_err'] = 'Address is too long ';
+        }
+  
+        if(empty($data['address_err']) && empty($data['contact_no_err']) && empty($data['dob_err']) && empty($data['nic_err']) && empty($data['name_err']) ){
+          if($this->collector_assistantModel->update_collector_assistants($data)){
+            $data['completed']='True';        
+            $this->view('collectors/collector_assistants_update',$data);
+          } else {
+            die('Something went wrong');
+          }
+        }
+        else{
+          $this->view('collectors/collector_assistants_update', $data);
+  
+        }
+      
+      }
+      else{
+        $ass = $this -> collector_assistantModel -> getCollectorAssisById($assisId);
+
+        $data = [
+          'assistant_id' => $assisId,
+          'name' => $ass->name,
+          'nic' => $ass->nic,
+          'dob'=> $ass->dob,
+          'contact_no'=> $ass->contact_no,
+          'address' => $ass->address,
+          'completed'=> '',
+
+          'name_err' => '',
+          'nic_err' =>'',
+          'dob_err'=> '',
+          'contact_no_err'=> '',
+          'address_err' => ''
+        ];
+        
+        $this->view('collectors/collector_assistants_update', $data);
+      }
+  
+      
+    }
   
    
   }
+
+
+
+
+
+  
+

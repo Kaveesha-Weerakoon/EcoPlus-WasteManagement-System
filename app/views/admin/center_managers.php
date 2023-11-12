14bc4d98df2e5a78eb6fad4a2f211081ad1f7b6b
Kaveesha Weerakoon <savindukaveesha11@gmail.com>
2023-11-12 23:34:13 +0530

    Center manager delete

--- app/views/admin/center_managers.php
+++ app/views/admin/center_managers.php
@@ -1,7 +1,7 @@
 <?php require APPROOT . '/views/inc/header.php'; ?>
 <div class="Admin_Center_Manager">
     <div class="Admin_Center_Manger_View">
-<div class="main">
+       <div class="main">
             <div class="main-top">
                 <a href="<?php echo URLROOT?>/admin">
                     <img class="back-button" src="<?php echo IMGROOT?>/Back.png" alt="">
@@ -59,8 +59,8 @@
                                            <td> <?php echo $center_manager->assigned_center_id?></td>
                                            <td class="cancel-open"><img src="<?php echo IMGROOT?>/view.png" alt=""></td>
                                            <td class="cancel-open"><img src="<?php echo IMGROOT?>/update.png" alt=""></td>
-                                           <td class="cancel-open"><img src="<?php echo IMGROOT?>/delete.png" alt=""></td>
-                                    </tr>
+                                           <td class="cancel-open"><a href="<?php echo URLROOT?>/admin/center_managers_delete_confirm/<?php echo $center_manager->user_id?>"><img src="<?php echo IMGROOT?>/delete.png" alt=""></a></td>
+                                   </tr>
                              <?php endforeach; ?>
                            </table>
                 
@@ -69,6 +69,41 @@
                 </div>
             </div>
         </div>
-</div>
+       </div> 
+       <?php if($data['confirm_delete']=='True') : ?>
+       <div class="delete_confirm">
+              <div class="popup" id="popup">
+                 <img src="<?php echo IMGROOT?>/trash.png" alt="">
+                 <?php
+                    if ($data['assigned']=='No') {
+                       echo "<h2>Delete this collector assistant?</h2>";
+                       echo "<p>This action will permanently delete this center manager</p>";
+                    }
+                    else{
+                       echo "<h2>This Action is prohibited</h2>";
+                       echo "<p>Center Manager is assisgned to a center</p>";
+                    }
+                 ?>
+                 <div class="btns">
+                    <?php
+                        if ($data['assigned']=='No') {
+                            echo '<a href="' . URLROOT . '/Admin/center_managers_delete/' . $data['center_manager_id'] . '"><button type="button" class="deletebtn">Delete</button></a>';
+                        }
+                    ?>                     
+                    <a href="<?php echo URLROOT?>/Admin/center_managers"><button type="button" class="cancelbtn" >Cancel</button></a>
+                  </div>
+               </div>
+       </div>
+       <?php endif; ?>
+       <?php if($data['success']=='True') : ?>
+           <div class="center_manager_success">
+                 <div class="popup" id="popup">
+                    <img src="<?php echo IMGROOT?>/check.png" alt="">
+                    <h2>Success!!</h2>
+                    <p>Center Manager has been deleted successfully</p>
+                    <a href="<?php echo URLROOT?>/admin/center_managers"><button type="button" >OK</button></a>
+                </div>
+           </div>
+        d<?php endif; ?>
 </div>
 <?php require APPROOT . '/views/inc/footer.php'; ?>

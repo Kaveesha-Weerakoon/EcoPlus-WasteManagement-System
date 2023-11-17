c63cab95223b144bc775cc8c377c0f268f495323
Kaveesha Weerakoon <savindukaveesha11@gmail.com>
2023-11-17 09:51:56 +0530

    customer_complain

--- app/views/customers/complains.php
+++ app/views/customers/complains.php
@@ -1,8 +1,9 @@
 <?php require APPROOT . '/views/inc/header.php'; ?>
 <div class="Customer_Complains">
+
     <div class="main">
       <div class="main-top">
-        <a href="<?php echo URLROOT?>/customers">
+      <a href="<?php echo URLROOT?>/customers">
           <img class="back_button" src="<?php echo IMGROOT?>/Back.png" alt="" />
         </a>
 
@@ -12,58 +13,60 @@
         </div>
       </div>
       <div class="main-bottom">
-        <div class="main-bottom-left">
-          <form class="main-bottom-left-component" action="<?php echo URLROOT;?>/customers/complains" method="post">
-            <h2>Make Complaints</h2>
-            <div class="line"></div>
-          
-            <div class="main-bottom-left-component-input">
-              <p>Name :</p>
-              <input type="text" name="name" value="<?php echo $data['name']; ?>"/>
-              <p class="error"><?php echo $data['name_err']; ?></p>
+        <div class="main-bottom-component" >
+          <form class="main-bottom-component-left" action="<?php echo URLROOT;?>/customers/complains" method="post">
+            <div class="main-bottom-component-left-topic">
+              <h2>Make a Complain</h2>
+              <div class="line"></div>
             </div>
 
-            <div class="main-bottom-left-component-input">
-              <p>Contact Number :</p>
-              <input type="text" name="contact_no" value="<?php echo $data['contact_no']; ?>"/>
-              <p class="error"><?php echo $data['contact_no_err']; ?></p>
+            <div class="main-bottom-component-left-component">
+              <input value="<?php echo $data['name']; ?>" type="text" name="name"  placeholder="Name">
+              <div class="err"><?php echo $data['name_err']; ?></div>
             </div>
 
-            <div class="main-bottom-left-component-input">
-              <p>Region :</p>
-              <input type="text" name="region" value="<?php echo $data['region']; ?>"/>
-              <p class="error"><?php echo $data['region_err']; ?></p>
+            <div class="main-bottom-component-left-component">
+              <input value="<?php echo $data['contact_no']; ?>" name="contact_no" type="text" placeholder="Contact Number">
+              <div class="err"><?php echo $data['contact_no_err']; ?></div>
             </div>
 
-            <div class="main-bottom-left-component-input">
-              <p>Subject :</p>
-              <input type="text" name="subject" value="<?php echo $data['subject']; ?>"/>
-              <p class="error"><?php echo $data['subject_err']; ?></p>
+            <div class="main-bottom-component-left-component">
+              <input type="text" value="<?php echo $data['region']; ?>" name="region" placeholder="District">
+              <div class="err"><?php echo $data['region_err']; ?></div>
             </div>
 
-            <div class="main-bottom-left-component-input A">
-              <p>Complaint :</p>
-              <input class="complaint-box" name="complain" type="text" value="<?php echo $data['complain']; ?>" />
-              <p class="error"><?php echo $data['complain_err']; ?></p>
+            <div class="main-bottom-component-left-component">
+              <input value="<?php echo $data['subject']; ?>" name="subject" type="text" placeholder="Subject">
+              <div  class="err"><?php echo $data['subject_err']; ?></div>
             </div>
 
-            <button type="submit">Submit</button>
-
+            <div class="main-bottom-component-left-component">
+              <input value="<?php echo $data['complain']; ?>" name="complain" type="text" placeholder="Complain" class="complain">
+              <div class="err"><?php echo $data['complain_err']; ?></div>
+            </div>
+            <div class="main-bottom-component-left-button">
+              <button type="submit">Make Complain</button>
+            </div>
           </form>
-        </div>
-        <div class="main-bottom-right">
+          <div class="main-bottom-component-right">
           <img src="<?php echo IMGROOT?>/makeComplaints.png" alt="" />
+          </div>
+
         </div>
+
       </div>
     </div>
     <?php if($data['completed']=='True') : ?>
-    <div class="complain-success">
-          <div class="complain-success-box">
-                  <h1>Complaint Successful</h1>
-                  <a href="<?php echo URLROOT?>/customers/complains"><button>Okay</button></a>
-          </div>
-    </div>
-    <?php endif; ?>
+            <div class="center_worker_success">
+                <div class="popup" id="popup">
+                    <img src="<?php echo IMGROOT?>/check.png" alt="">
+                    <h2>Success!!</h2>
+                    <p>Complain has been reported successfully</p>
+                    <a href="<?php echo URLROOT?>/customers/history_complains"><button type="button" >OK</button></a>
+
+                </div>
+            </div>
+        <?php endif; ?>
 </div>
 
 <?php require APPROOT . '/views/inc/footer.php'; ?>

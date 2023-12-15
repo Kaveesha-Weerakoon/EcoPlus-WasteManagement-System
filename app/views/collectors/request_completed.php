54035dc6a52139ec5d145d94fff2ce2a0d901cf3
Kaveesha Weerakoon <savindukaveesha11@gmail.com>
2023-12-15 10:54:02 +0530

    push

--- app/views/collectors/request_completed.php
+++ app/views/collectors/request_completed.php
@@ -1,172 +1,172 @@
 <?php require APPROOT . '/views/inc/header.php'; ?>
 <div class="Collector_Main">
     <div class="Collector_Request_Top">
-
-
-        <div class="main">
-            <div class="main-left">
-                <div class="main-left-top">
-                    <img src="<?php echo IMGROOT?>/Logo_No_Background.png" alt="">
-                    <h1>Eco Plus</h1>
-                </div>
-
-                <div class="main-left-middle">
-                    <a href="<?php echo URLROOT?>/collectors">
-                        <div class="main-left-middle-content ">
-                            <div class="main-left-middle-content-line1"></div>
-                            <img src="<?php echo IMGROOT?>/Home.png" alt="">
-                            <h2>Dashboard</h2>
-                        </div>
-                    </a>
-                    <a href="">
-                        <div class="main-left-middle-content current ">
-                            <div class="main-left-middle-content-line"></div>
-                            <img src="<?php echo IMGROOT?>/Request.png" alt="">
-                            <h2>Requests</h2>
-                        </div>
-                    </a>
-                    <a href="<?php echo URLROOT?>/collectors/collector_assistants">
-                        <div class="main-left-middle-content Collector">
-                            <div class="main-left-middle-content-line1"></div>
-                            <img src="<?php echo IMGROOT?>/CollectorAssis.png" alt="">
-                            <h2>Collector Assistants</h2>
-                        </div>
-                    </a>
-                    <a href="<?php echo URLROOT?>/collectors/editprofile">
-                        <div class="main-left-middle-content ">
-                            <div class="main-left-middle-content-line1"></div>
-                            <img src="<?php echo IMGROOT?>/EditProfile.png" alt="">
-                            <h2>Edit Profile</h2>
-                        </div>
-                    </a>
-
-                </div>
-                <div class="main-left-bottom">
-                    <a href="<?php echo URLROOT?>/collectors/logout">
-                        <div class="main-left-bottom-content">
-                            <img src="<?php echo IMGROOT?>/logout.png" alt="">
-                            <p>Log out</p>
-                        </div>
-                    </a>
-                </div>
-            </div>
-            <div class="main-right">
-                <div class="main-right-top">
-                    <div class="main-right-top-one">
-                        <div class="main-right-top-one-search">
-                            <img src="<?php echo IMGROOT?>/Search.png" alt="">
-                            <input id="searchInput" type="text" placeholder="Search">
-                        </div>
-
-                        <div class="main-right-top-one-content">
-                            <p><?php echo $_SESSION['collector_name']?></p>
-                            <img src="<?php echo IMGROOT?>/img_upload/collector/<?php echo $_SESSION['collector_profile']?>"
-                                alt="">
-                        </div>
+        <div class="Collector_request_completed">
+            <div class="main">
+                <div class="main-left">
+                    <div class="main-left-top">
+                        <img src="<?php echo IMGROOT?>/Logo_No_Background.png" alt="">
+                        <h1>Eco Plus</h1>
                     </div>
-                    <div class="main-right-top-two">
-                        <h1>Requests</h1>
-                    </div>
-                    <div class="main-right-top-three">
-                        <a href="<?php echo URLROOT?>/collectors/request_assinged">
-                            <div class="main-right-top-three-content">
-                                <p>Assigned</p>
-                                <div class="line1"></div>
+
+                    <div class="main-left-middle">
+                        <a href="<?php echo URLROOT?>/collectors">
+                            <div class="main-left-middle-content ">
+                                <div class="main-left-middle-content-line1"></div>
+                                <img src="<?php echo IMGROOT?>/Home.png" alt="">
+                                <h2>Dashboard</h2>
                             </div>
                         </a>
-                        <a href="<?php echo URLROOT?>/collectors/request_completed">
-                            <div class="main-right-top-three-content">
-                                <p><b style="color: #1B6652;">Completed</b></p>
-                                <div class="line"></div>
+                        <a href="">
+                            <div class="main-left-middle-content current ">
+                                <div class="main-left-middle-content-line"></div>
+                                <img src="<?php echo IMGROOT?>/Request.png" alt="">
+                                <h2>Requests</h2>
                             </div>
                         </a>
-                        <a href="<?php echo URLROOT?>/collectors/request_cancelled">
-                            <div class="main-right-top-three-content">
-                                <p>Cancelled</p>
-                                <div class="line1"></div>
+                        <a href="<?php echo URLROOT?>/collectors/collector_assistants">
+                            <div class="main-left-middle-content Collector">
+                                <div class="main-left-middle-content-line1"></div>
+                                <img src="<?php echo IMGROOT?>/CollectorAssis.png" alt="">
+                                <h2>Collector Assistants</h2>
+                            </div>
+                        </a>
+                        <a href="<?php echo URLROOT?>/collectors/editprofile">
+                            <div class="main-left-middle-content ">
+                                <div class="main-left-middle-content-line1"></div>
+                                <img src="<?php echo IMGROOT?>/EditProfile.png" alt="">
+                                <h2>Edit Profile</h2>
+                            </div>
+                        </a>
+
+                    </div>
+                    <div class="main-left-bottom">
+                        <a href="<?php echo URLROOT?>/collectors/logout">
+                            <div class="main-left-bottom-content">
+                                <img src="<?php echo IMGROOT?>/logout.png" alt="">
+                                <p>Log out</p>
                             </div>
                         </a>
                     </div>
-                    <div class="main-right-top-four">
-                        <div class="main-right-top-four-left">
-                            <p>Date</p>
-                            <input type="date" id="selected-date">
-                            <button onclick="loadLocations()">Filter</button>
+                </div>
+                <div class="main-right">
+                    <div class="main-right-top">
+                        <div class="main-right-top-one">
+                            <div class="main-right-top-one-search">
+                                <img src="<?php echo IMGROOT?>/Search.png" alt="">
+                                <input id="searchInput" type="text" placeholder="Search">
+                            </div>
+
+                            <div class="main-right-top-one-content">
+                                <p><?php echo $_SESSION['collector_name']?></p>
+                                <img src="<?php echo IMGROOT?>/img_upload/collector/<?php echo $_SESSION['collector_profile']?>"
+                                    alt="">
+                            </div>
                         </div>
-                        <div class="main-right-top-four-right">
-                            <div class="main-right-top-four-component" style="background-color: #ecf0f1" id="tables">
-                                <img src="<?php echo IMGROOT?>/cells.png" alt="">
-                                <p>Tables</p>
+                        <div class="main-right-top-two">
+                            <h1>Requests</h1>
+                        </div>
+                        <div class="main-right-top-three">
+                            <a href="<?php echo URLROOT?>/collectors/request_assinged">
+                                <div class="main-right-top-three-content">
+                                    <p>Assigned</p>
+                                    <div class="line1"></div>
+                                </div>
+                            </a>
+                            <a href="<?php echo URLROOT?>/collectors/request_completed">
+                                <div class="main-right-top-three-content">
+                                    <p><b style="color: #1B6652;">Completed</b></p>
+                                    <div class="line"></div>
+                                </div>
+                            </a>
+                            <a href="<?php echo URLROOT?>/collectors/request_cancelled">
+                                <div class="main-right-top-three-content">
+                                    <p>Cancelled</p>
+                                    <div class="line1"></div>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="main-right-top-four">
+                            <div class="main-right-top-four-left">
+                                <p>Date</p>
+                                <input type="date" id="selected-date">
+                                <button onclick="loadLocations()">Filter</button>
                             </div>
-                            <div class="main-right-top-four-component" id="maps">
-                                <img src="<?php echo IMGROOT?>/map.png" alt="">
-                                <p>Maps</p>
-                            </div>
+                            <div class="main-right-top-four-right">
+                                <div class="main-right-top-four-component" style="background-color: #ecf0f1"
+                                    id="tables">
+                                    <img src="<?php echo IMGROOT?>/cells.png" alt="">
+                                    <p>Tables</p>
+                                </div>
+                                <div class="main-right-top-four-component" id="maps">
+                                    <img src="<?php echo IMGROOT?>/map.png" alt="">
+                                    <p>Maps</p>
+                                </div>
 
 
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="main-right-bottom">
-                <?php if(!empty($data['completed_requests'])) : ?>
                     <div class="main-right-bottom">
-                        <div class="main-right-bottom-one" id="main-right-bottom-one">
-                            <div class="main-right-bottom-top">
-                                <table class="table">
-                                    <tr class="table-header">
-                                        <th>Req ID</th>
-                                        <th>Date</th>
-                                        <th>Time</th>
-                                        <th>Customer</th>
-                                        <th>C ID</th>
-                                        <th>Contact No</th>
-                                        <th>Instructions</th>
-                                        <th>Complete</th>
-                                        <th>creidt_amount</th>
-                                    </tr>
-                                </table>
+                        <?php if(!empty($data['completed_requests'])) : ?>
+                        <div class="main-right-bottom">
+                            <div class="main-right-bottom-one" id="main-right-bottom-one">
+                                <div class="main-right-bottom-top">
+                                    <table class="table">
+                                        <tr class="table-header">
+                                            <th>Req ID</th>
+                                            <th>Date</th>
+                                            <th>Time</th>
+                                            <th>Customer</th>
+                                            <th>C ID</th>
+                                            <th>Contact No</th>
+                                            <th>Instructions</th>
+                                            <th>Complete</th>
+                                            <th>Credits</th>
+                                        </tr>
+                                    </table>
+                                </div>
+                                <div class="main-right-bottom-down">
+
+                                    <table class="table">
+                                        <?php foreach($data['completed_requests'] as $request) : ?>
+                                        <tr class="table-row">
+                                            <td>R<?php echo $request->req_id?></td>
+                                            <td><?php  echo $request->date?></td>
+                                            <td><?php  echo $request->time?></td>
+                                            <td><?php  echo $request->name?></td>
+                                            <td><?php  echo $request->customer_id?></td>
+                                            <td><?php  echo $request->contact_no?></td>
+                                            <td><?php  echo $request->instructions?></td>
+                                            <td><img onclick="view_request_details(<?php echo htmlspecialchars(json_encode($request), ENT_QUOTES, 'UTF-8') ?>)"
+                                                    src="<?php echo IMGROOT?>/view.png" alt=""></td>
+                                            <td><?php  echo $request->credit_amount?></td>
+                                        </tr>
+                                        <?php endforeach; ?>
+                                    </table>
+
+                                </div>
                             </div>
-                            <div class="main-right-bottom-down">
+                            <div class="main-right-bottom-two" id="main-right-bottom-two">
+                                <div class="map-locations" id="map-loaction">
 
-                                <table class="table">
-                                    <?php foreach($data['completed_requests'] as $request) : ?>
-                                    <tr class="table-row">
-                                        <td>R<?php echo $request->req_id?></td>
-                                        <td><?php  echo $request->date?></td>
-                                        <td><?php  echo $request->time?></td>
-                                        <td><?php  echo $request->name?></td>
-                                        <td><?php  echo $request->customer_id?></td>
-                                        <td><?php  echo $request->contact_no?></td>
-                                        <td><?php  echo $request->instructions?></td>
-                                        <td><img onclick="view_request_details(<?php echo htmlspecialchars(json_encode($request), ENT_QUOTES, 'UTF-8') ?>)"
-                                            src="<?php echo IMGROOT?>/view.png" alt=""></td>
-                                        <td><?php  echo $request->credit_amount?></td>
-                                    </tr>
-                                    <?php endforeach; ?>
-                                </table>
-
+                                </div>
                             </div>
                         </div>
-                        <div class="main-right-bottom-two" id="main-right-bottom-two">
-                            <div class="map-locations" id="map-loaction">
-
+                        <?php else: ?>
+                        <div class="main-right-bottom-three">
+                            <div class="main-right-bottom-three-content">
+                                <img src="<?php echo IMGROOT?>/Center_Manager_Request_Assign_Empty.jpg" alt="">
+                                <h1>You have No completed Requests</h1>
                             </div>
                         </div>
+                        <?php endif; ?>
                     </div>
-                    <?php else: ?>
-                    <div class="main-right-bottom-three">
-                        <div class="main-right-bottom-three-content">
-                            <img src="<?php echo IMGROOT?>/Center_Manager_Request_Assign_Empty.jpg" alt="">
-                            <h1>You have No completed Requests</h1>
-                        </div>
-                    </div>
-                    <?php endif; ?>
-                </div>
 
                     <div class="request-details-pop" id="request-details-popup-box">
                         <div class="request-details-pop-form">
-                            <img src="<?php echo IMGROOT?>/close_popup.png" alt="" class="request-details-pop-form-close"
-                                id="request-details-pop-form-close">
+                            <img src="<?php echo IMGROOT?>/close_popup.png" alt=""
+                                class="request-details-pop-form-close" id="request-details-pop-form-close">
                             <div class="request-details-pop-form-top">
                                 <div class="request-details-topic">Request ID R <div id="req_id3"></div>
                                 </div>
@@ -198,6 +198,7 @@
             </div>
         </div>
     </div>
+</div>
 </div>
 
 
@@ -395,7 +396,5 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 
 });
-
 </script>
-<?php require APPROOT . '/views/inc/footer.php'; ?>
 <?php require APPROOT . '/views/inc/footer.php'; ?>

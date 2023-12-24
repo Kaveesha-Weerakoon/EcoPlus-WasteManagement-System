6668644eb8ad6b1b567c344a36b80689bdf15ea0
Nimalsh <nimalshapg@gmail.com>
2023-12-24 16:55:41 +0530

    side bar2

--- app/views/collectors/collector_sidebar/side_bar.php
+++ app/views/collectors/collector_sidebar/side_bar.php
@@ -17,7 +17,7 @@
             </div>
             <h3>Requests</h3>
         </div>
-        <div class="main-left-middle-content" onclick="redirect_history()" id="history">
+        <div class="main-left-middle-content" onclick="redirect_history()" id="Collector_Assistants">
             <div class=" main-left-middle-content-icon">
                 <i class='bx bx-timer'></i>
             </div>

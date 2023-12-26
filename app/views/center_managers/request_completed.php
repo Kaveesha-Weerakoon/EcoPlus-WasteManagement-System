c6821e46e78b37f85d7c40f2cd567feff2d5a392
DinithiDinushika <dinithidinu689@gmail.com>
2023-12-26 14:31:27 +0530

    request complete popups

--- app/views/center_managers/request_completed.php
+++ app/views/center_managers/request_completed.php
@@ -101,9 +101,9 @@
                                     <td><?php  echo $request->name?></td>
                                     <td class="cancel-open">
                                         <img src="<?php echo IMGROOT ?>/assign.png"
-                                            <?php if ($request->type === 'completed') { ?>onclick="view_collector('<?php echo $request->collector_image; ?>', '<?php echo $request->collector_id; ?>', '<?php echo $request->name; ?>', 
+                                            onclick="view_collector('<?php echo $request->collector_image; ?>', '<?php echo $request->collector_id; ?>', '<?php echo $request->name; ?>', 
                                             '<?php echo $request->collector_contact_no; ?>', '<?php echo $request->collector_vehicle_no; ?>', '<?php echo $request->collector_vehicle_type; ?>')"
-                                            <?php } ?> alt="">
+                                         alt="">
                                     </td>
                                     <td><img onclick="viewLocation(<?php echo $request->lat; ?>, <?php echo $request->longi; ?>)"
                                             class="add" src="<?php echo IMGROOT?>/location.png" alt=""></td>
@@ -114,7 +114,7 @@
                                             onclick="view_collect_details(<?php echo htmlspecialchars(json_encode($request), ENT_QUOTES, 'UTF-8') ?>)"
                                             src="<?php echo IMGROOT?>/view.png" alt="">
                                     </td>
-                                    <td><button>Confirmed</button></td>
+                                    <td><button class="confirmed_button">Confirmed</button></td>
 
 
 
@@ -147,51 +147,123 @@
                         </div>
                     </div>
                 </div>
-                <div class="request-details-pop" id="request-details-popup-box">
-                    <div class="request-details-pop-form">
-                        <img src="<?php echo IMGROOT?>/close_popup.png" alt="" class="request-details-pop-form-close"
-                            id="request-details-pop-form-close">
-                        <div class="request-details-pop-form-top">
-                            <div class="request-details-topic">Request ID R <div id="req_id3"></div>
+
+                <div class="collect-details-pop" id="collect-details-popup-box">
+                    <div class="collect-details-pop-form">
+                        <img src="<?php echo IMGROOT?>/close_popup.png" alt="" class="collect-details-pop-form-close"
+                            id="collect-details-pop-form-close">
+                        <div class="collect-details-pop-form-top">
+                            <div class="collect-details-topic">Completed Details<div id="req_id3"></div>
                             </div>
                         </div>
 
-                        <div class="request-details-pop-form-content">
-                            <div class="personal-details-right-labels">
-                                <span>Customer Id</span><br>
-                                <span>Name</span><br>
-                                <span>Date</span><br>
-                                <span>Time</span><br>
-                                <span>Contact No</span><br>
-                                <span>Instructions</span><br>
+                        <div class="collect-details-pop-form-content">
+                            <div class="collect-details-pop-form-content-labels">
+                                <h3>Polythene Quantity</h3>
+                                <h3>Plastic Quantity</h3>
+                                <h3>Glass Quantity </h3>
+                                <h3>Paper Waste Quantity</h3>
+                                <h3>Electronic Waste Quantity </h3>
+                                <h3>Metals Quantity</h3>
+                                <h3>Details</h3>
                             </div>
-                            <div class="personal-details-right-values">
-                                <span id="req_id2"></span><br>
-                                <span id="req_name"></span><br>
-                                <span id="req_date"></span><br>
-                                <span id="req_time"></span><br>
-                                <span id="req_contactno"></span><br>
-                                <span id="instructions"></span><br>
+                            <div class="collect-details-pop-form-content-right-values">
+                                <div class="collect-details-pop-form-content-right-values-cont">
+                                    <h3 id="Polythene_Quantity"></h3>
+                                    <h3>&nbsp Kg</h3>
+                                </div>
+                                <div class="collect-details-pop-form-content-right-values-cont">
+                                    <h3 id="Plastic_Quantity"></h3>
+                                    <h3>&nbsp Kg</h3>
+                                </div>
+                                <div class="collect-details-pop-form-content-right-values-cont">
+                                    <h3 id="Glass_Quantity"></h3>
+                                    <h3>&nbsp Kg</h3>
+                                </div>
+                                <div class="collect-details-pop-form-content-right-values-cont">
+                                    <h3 id="Paper_Waste_Quantity"></h3>
+                                    <h3>&nbsp Kg</h3>
+                                </div>
+                                <div class="collect-details-pop-form-content-right-values-cont">
+                                    <h3 id="Electronic_Waste_Quantity"></h3>
+                                    <h3>&nbsp Kg</h3>
+                                </div>
+                                <div class="collect-details-pop-form-content-right-values-cont">
+                                    <h3 id="Metals_Quantity"></h3>
+                                    <h3>&nbsp Kg</h3>
+                                </div>
+                                <div class="collect-details-pop-form-content-right-values-cont">
+                                    <h3 id="Note"></h3>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
+
+                <div class="overlay" id="overlay"></div>
+
+                <div class="personal-details-popup-box" id="personal-details-popup-box">
+                    <div class="personal-details-popup-form" id="popup">
+                        <img src="<?php echo IMGROOT?>/close_popup.png" alt="" class="personal-details-popup-form-close"
+                            id="personal-details-popup-form-close">
+                        <center>
+                            <div class="personal-details-topic">Collector Details</div>
+                        </center>
+
+                        <div class="personal-details-popup">
+                            <div class="personal-details-left">
+                                <img id="collector_profile_img" src="<?php echo IMGROOT?>/img_upload/collector/?>"
+                                    class="profile-pic" alt="">
+                                <p>Collector ID: <span id="collector_id">C</span></p>
+                            </div>
+                            <div class="personal-details-right">
+                                <div class="personal-details-right-labels">
+                                    <span>Name</span><br>
+                                    <span>Contact No</span><br>
+                                    <span>Vehicle No</span><br>
+                                    <span>Vehicle Type</span><br>
+
+                                </div>
+                                <div class="personal-details-right-values">
+                                    <span id="collector_name"></span><br>
+                                    <span id="collector_conno"></span><br>
+                                    <span id="collector_vehicle_no"></span><br>
+                                    <span id="collector_vehicle_type"></span><br>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
             </div>
         </div>
     </div>
 </div>
 <script>
-function view_request_details(request) {
+function view_collector(image, col_id, name, contact_no, type, vehno) {
+    var locationPop = document.getElementById('personal-details-popup-box');
+    locationPop.classList.add('active');
+    document.getElementById('overlay').style.display = "flex";
+    document.getElementById('collector_profile_img').src = '<?php echo IMGROOT ?>/img_upload/collector/' + image;
+    document.getElementById('collector_id').innerText = col_id;
+    document.getElementById('collector_name').innerText = name;
+    document.getElementById('collector_conno').innerText = contact_no;
+    document.getElementById('collector_vehicle_no').innerText = vehno;
+    document.getElementById('collector_vehicle_type').innerText = type;
+}
 
-    document.getElementById('request-details-popup-box').style.display = "flex";
-    document.getElementById('req_id3').innerText = request.req_id;
-    document.getElementById('req_id2').innerText = request.customer_id;
-    document.getElementById('req_name').innerText = request.name;
-    document.getElementById('req_date').innerText = request.date;
-    document.getElementById('req_time').innerText = request.time;
-    document.getElementById('req_contactno').innerText = request.contact_no;
-    document.getElementById('instructions').innerText = request.instructions;
+function view_collect_details(request) {
+    var locationPop = document.getElementById('collect-details-popup-box');
+    locationPop.classList.add('active');
+    document.getElementById('overlay').style.display = "flex";
 
+    document.getElementById('Polythene_Quantity').innerText = request.Polythene;
+    document.getElementById('Plastic_Quantity').innerText = request.Plastic;
+    document.getElementById('Glass_Quantity').innerText = request.Glass;
+    document.getElementById('Paper_Waste_Quantity').innerText = request.Paper_Waste;
+    document.getElementById('Electronic_Waste_Quantity').innerText = request.Electronic_Waste;
+    document.getElementById('Metals_Quantity').innerText = request.Metals;
+    document.getElementById('Note').innerText = request.note;
 }
 
 function initMap(latitude = 7.4, longitude = 81.00000000) {
@@ -266,12 +338,26 @@ function searchTable() {
 
 }
 
+document.getElementById('searchInput').addEventListener('input', searchTable);
 document.addEventListener("DOMContentLoaded", function() {
-    const close_request_details = document.getElementById("request-details-pop-form-close");
+    const close_collector = document.getElementById("personal-details-popup-form-close");
+    const collector_view = document.getElementById("personal-details-popup-box");
+    const close_view = document.getElementById("collect-details-pop-form-close")
 
-    close_request_details.addEventListener("click", function() {
-        document.getElementById('request-details-popup-box').style.display = "none";
+    close_collector.addEventListener("click", function() {
+        collector_view.classList.remove('active');
+        document.getElementById('overlay').style.display = "none";
     });
+
+    close_view.addEventListener("click", function() {
+        var locationPop = document.getElementById('collect-details-popup-box');
+        locationPop.classList.remove('active');
+        document.getElementById('overlay').style.display = "none";
+
+    });
+
 });
+
+
 </script>
 <?php require APPROOT . '/views/inc/footer.php'; ?>

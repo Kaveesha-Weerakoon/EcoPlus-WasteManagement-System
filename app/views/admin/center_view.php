3a483cd4e81b1165e6c816f46100ff3e2a42fe90
DinithiDinushika <dinithidinu689@gmail.com>
2023-12-29 02:17:08 +0530

    center add

--- app/views/admin/center_view.php
+++ app/views/admin/center_view.php
@@ -52,7 +52,7 @@
                             <h1>Centers</h1>
                         </div>
                         <div class="main-right-top-three">
-                            <a href="">
+                            <a href="<?php echo URLROOT?>/Admin/Center">
                                 <div class="main-right-top-three-content">
                                     <p><b style="color:#1ca557;">View</b></p>
                                     <div class="line"  style="background-color: #1ca557;"></div>
@@ -67,37 +67,7 @@
 
                         </div>
                     </div>
-                    <!-- <div class="main-top">
-                        <a href="<?php echo URLROOT?>/admin">
-                            <img class="back-button" src="<?php echo IMGROOT?>/Back.png" alt="">
-                        </a>
-
-                        <div class="main-top-component">
-                            <p>Admin</p>
-                            <img src="<?php echo IMGROOT?>/Requests Profile.png" alt="">
-                        </div>
-                    </div>
-                   
-                    <div class="main-bottom-top">
-                        <div class="main-right-top-two">
-                            <h1>Centers</h1>
-                        </div>
-                        <div class="main-right-top-three">
-                            <a href="">
-                                <div class="main-right-top-three-content">
-                                    <p><b style="color: #1B6652;">View</b></p>
-                                    <div class="line"></div>
-                                </div>
-                            </a>
-                            <a href="<?php echo URLROOT?>/admin/center_add">
-                                <div class="main-right-top-three-content">
-                                    <p>Add</p>
-                                    <div class="line1"></div>
-                                </div>
-                            </a>
-
-                        </div>
-                    </div> -->
+                    
                     <div class="main-right-bottom">
                         <div class="main-right-bottom-top ">
                             <table class="table">

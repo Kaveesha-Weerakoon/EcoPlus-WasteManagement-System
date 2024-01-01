8f78127c82422be322a579027c4679b14b22f1a6
DinithiDinushika <dinithidinu689@gmail.com>
2024-01-02 03:24:56 +0530

    center manager dashboard

--- app/views/center_managers/index.php
+++ app/views/center_managers/index.php
@@ -4,51 +4,201 @@
     <div class="CenterManager_Dashboard">
         <div class="main">
             <?php require APPROOT . '/views/center_managers/centermanager_sidebar/side_bar.php'; ?>
-            <!-- <div class="main-left">
-                <div class="main-left-top">
-                    <img src="<?php echo IMGROOT?>/Logo_No_Background.png" alt="">
-                    <h1>Eco Plus</h1>
-                </div>
-                <div class="main-left-middle">
-                    <a href="./CenterManager_DashBoard.html">
-                        <div class="main-left-middle-content current">
-                            <div class="main-left-middle-content-line"></div>
-                            <img src="<?php echo IMGROOT?>/Home.png" alt="">
-                            <h2>Dashboard</h2>
-                        </div>
-                    </a>
-                    <a href="<?php echo URLROOT?>/centermanagers/request_incomming">
-                        <div class="main-left-middle-content">
-                            <div class="main-left-middle-content-line1"></div>
-                            <img src="<?php echo IMGROOT?>/Request.png" alt="">
-                            <h2>Requests</h2>
-                        </div>
-                    </a>
-                    <a href="">
-                        <div class="main-left-middle-content Collector">
-                            <div class="main-left-middle-content-line1"></div>
-                            <img src="<?php echo IMGROOT?>/Center.png" alt="">
-                            <h2>Center Waste Management</h2>
-                        </div>
-                    </a>
-                    <a href="<?php echo URLROOT?>/centermanagers/editprofile">
-                        <div class="main-left-middle-content">
-                            <div class="main-left-middle-content-line1"></div>
-                            <img src="<?php echo IMGROOT?>/EditProfile.png" alt="">
-                            <h2>Edit Profile</h2>
-                        </div>
-                    </a>
-                </div>
-                <div class="main-left-bottom">
-                     <a href="<?php echo URLROOT?>/centermanagers/logout" class="logout-ancor">
-                        <div class="main-left-bottom-content">                      
-                            <img src="<?php echo IMGROOT?>/logout.png" alt="">
-                            <p>Log out</p> 
-                        </div>                  
-                      </a>
-                </div>
-            </div> -->
+
             <div class="main-right">
+                <!-- <div class="main-right-top">
+                    <div class="main-right-top-search">
+                        <i class='bx bxl-sketch'></i> <input type="text" placeholder="Welcome Back !" id="searchInput"
+                            readonly oninput="highlightMatchingText()">
+                    </div>
+                    <div class="main-right-top-notification" id="notification">
+                        <i class='bx bx-bell'></i>
+                        <?php if (!empty($data['notification'])) : ?>
+                        <div class="dot"></div>
+                        <?php endif; ?>
+                    </div>
+                    <div id="notification_popup" class="notification_popup">
+                        <h1>Notifications</h1>
+                        <div class="notification_cont">
+                            <?php foreach($data['notification'] as $notification) : ?>
+
+                            <div class="notification">
+                                <div class="notification-green-dot">
+
+                                </div>
+                                <?php echo $notification->notification?>
+                            </div>
+                            <?php endforeach; ?>
+
+                        </div>
+                        <form class="mark_as_read" method="post" action="<?php echo URLROOT;?>/customers/">
+                            <i class='bx bx-signal-4'></i>
+                            <button type="submit">Mark all as read</button>
+                        </form>
+
+                    </div>
+                    <div class="main-right-top-profile">
+                        <img src="<?php echo IMGROOT?>/img_upload/customer/<?php echo $_SESSION['customer_profile']?>"
+                            alt="">
+                        <div class="main-right-top-profile-cont">
+                            <h3><?php echo $_SESSION['user_name']?></h3>
+                            <p>ID : C <?php echo $_SESSION['user_id']?></p>
+                        </div>
+                    </div>
+                </div> -->
+                <div class="main-right-top">
+                    <div class="main-right-top-search">
+                        <i class='bx bx-search-alt-2'></i>
+                        <input type="text" placeholder="Search">
+                    </div>
+                    <div class="main-right-top-notification" id="notification">
+                        <i class='bx bx-bell'></i>
+                        <div class="dot"></div>
+                    </div>
+                    <div id="notification_popup" class="notification_popup">
+                        <h1>Notifications</h1>
+                        <div class="notification">
+                            <div class="notification-green-dot">
+
+                            </div>
+                            Request 1232 Has been Cancelled
+                        </div>
+                        <div class="notification">
+                            <div class="notification-green-dot">
+
+                            </div>
+                            Request 1232 Has been Assigned
+                        </div>
+                        <div class="notification">
+                            <div class="notification-green-dot">
+
+                            </div>
+                            Request 1232 Has been Cancelled
+                        </div>
+
+
+                    </div>
+                    <div class="main-right-top-profile">
+                        <img src="<?php echo IMGROOT?>/img_upload/center_manager/<?php echo $_SESSION['cm_profile']?>" alt="">
+                        <div class="main-right-top-profile-cont">
+                            <h3><?php echo $_SESSION['center_manager_name']?></h3>
+                            <p>ID : CM <?php echo $_SESSION['center_manager_id']?></p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="main-right-bottom">
+                    <div class="main-right-bottom-one">
+                        <div class="main-right-bottom-one-left">
+                            <div class="left">
+                                <h1>Total Balance</h1>
+                                <h3>+Eco 26.23 </h3>
+                                <p>Last Update</p>
+                                <button onclick="redirect_transfercredit()">Transfer Credit</button>
+
+                            </div>
+
+                            <div class="right">
+                                <h1>Eco<span class="main-credit"></span> </h1>
+                                <h3>WALLET AMOUNT</h3>
+                            </div>
+                        </div>
+                        <div class="main-right-bottom-one-right">
+
+                            <canvas id="myChart" width="700" height="300"></canvas>
+                        </div>
+                    </div>
+                    <div class="main-right-bottom-two">
+                        <div class="main-right-bottom-two-cont A" id="credit_per_waste_quantity">
+                            <div class="icon_container">
+                                <i class='bx bx-dollar-circle'></i>
+                            </div>
+                            <h3>Collectors</h3>
+                        </div>
+                        <div class="main-right-bottom-two-cont A">
+                            <div class="icon_container">
+                                <i class='bx bx-money-withdraw'></i>
+                            </div>
+                            <h3>Center Workers</h3>
+                        </div>
+                        <div class="main-right-bottom-two-cont A">
+                            <div class="icon_container">
+                                <i class='bx bxs-bank'></i>
+                            </div>
+                            <h3>Discount Agents</h3>
+                        </div>
+                        <div class="main-right-bottom-two-cont A" onclick="redirect_complains()">
+                            <div class="icon_container">
+                                <i class='bx bx-donate-heart'></i>
+                            </div>
+                            <h3>Complaints</h3>
+                        </div>
+
+                    </div>
+                    <div class="main-right-bottom-three">
+                        <div class="main-right-bottom-three-left">
+                            <!-- <h1>Recent Transactions</h1>
+
+                            <?php
+                                 $transaction_history = $data['transaction_history'];
+                                 $limited_transactions = array_slice($transaction_history, 0, 3);
+
+                                 foreach ($limited_transactions as $transaction):
+                            ?>
+                            <div class="main-right-bottom-three-left-cont">
+                                <?php if ($transaction->sender_id == $_SESSION['user_id']): ?>
+                                <img class="td-pro_pic"
+                                    src="<?php echo (empty($transaction->receiver_image) || !file_exists('C:/xampp/htdocs/ecoplus/public/img/img_upload/customer/' . $transaction->receiver_image) ) ? IMGROOT . '/img_upload/customer/Profile.png': IMGROOT . '/img_upload/customer/' . $transaction->receiver_image; ?>"
+                                    alt="">
+                                <?php else: ?>
+                                <img class="td-pro_pic"
+                                    src="<?php echo (empty($transaction->sender_image) || !file_exists('C:/xampp/htdocs/ecoplus/public/img/img_upload/customer/'. $transaction->sender_image) ) ? IMGROOT . '/img_upload/customer/Profile.png': IMGROOT . '/img_upload/customer/' . $transaction->sender_image; ?>"
+                                    alt="">
+                                <?php endif; ?>
+                                <h3><?php if ($transaction->sender_id == $_SESSION['user_id']): ?>
+                                    C <?php echo $transaction->receiver_id; ?>
+                                    <?php else: ?>
+                                    C <?php echo $transaction->sender_id; ?>
+                                    <?php endif; ?>
+                                </h3>
+                                <h2
+                                    style="color: <?php echo ($transaction->sender_id == $_SESSION['user_id']) ? '#F13E3E' : '#1ca557'; ?>;">
+                                    <?php
+                                          echo ($transaction->sender_id == $_SESSION['user_id']) ? "-Eco " : "+Eco ";
+                                          echo $transaction->transfer_amount;
+                                    ?>
+                                </h2>
+                            </div>
+                            <?php endforeach; ?> -->
+                            
+                        </div>
+                        <div class="main-right-bottom-three-right">
+                            <div class="main-right-bottom-three-right-left">
+                                <h1>Requests Satisfied</h1>
+                                <div class="main-right-bottom-three-right-cont">
+                                    <div class="circular-progress">
+                                        <span class="progress-value">
+                                            0 %
+                                        </span>
+                                    </div>
+                                </div>
+                                <button onclick="redirect_requests()">
+                                    Request Now
+                                </button>
+                            </div>
+                            <div class="main-right-bottom-three-right-right">
+                                <h1>Centers</h1>
+                                <div class="map" id="map"></div>
+
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+            </div>
+            
+            <!-- <div class="main-right">
                 <div class="main-right-left">
                     <div class="main-right-left-one">
                         <div class="main-right-left-one-text">
@@ -98,7 +248,7 @@
                         <img src="<?php echo IMGROOT?>/Dashboard-Man.jpg" alt="">
                     </div>
                 </div>
-            </div>
+            </div> -->
         </div>
     </div>
  </div>

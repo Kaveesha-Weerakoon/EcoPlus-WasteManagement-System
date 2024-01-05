18d87c72e4894d907238c33d6f942aceec8355e7
Kaveesha Weerakoon <savindukaveesha11@gmail.com>
2024-01-05 21:00:55 +0530

    a

--- app/views/center_managers/request_assinged.php
+++ app/views/center_managers/request_assinged.php
@@ -5,117 +5,10 @@
     <div class="CenterManager_Request_Main">
         <div class="CenterManager_Request_Assinged">
             <div class="main">
-            <?php require APPROOT . '/views/center_managers/centermanager_sidebar/side_bar.php'; ?>
-                <!-- <div class="main-left">
-                    <div class="main-left-top">
-                        <img src="<?php echo IMGROOT?>/Logo_No_Background.png" alt="">
-                        <h1>Eco Plus</h1>
-                    </div>
-                    <div class="main-left-middle">
-                        <a href="<?php echo URLROOT?>/centermanagers">
-                            <div class="main-left-middle-content ">
-                                <div class="main-left-middle-content-line1"></div>
-                                <img src="<?php echo IMGROOT?>/Home.png" alt="">
-                                <h2>Dashboard</h2>
-                            </div>
-                        </a>
-                        <a href="">
-                            <div class="main-left-middle-content current">
-                                <div class="main-left-middle-content-line"></div>
-                                <img src="<?php echo IMGROOT?>/Request.png" alt="">
-                                <h2>Requests</h2>
-                            </div>
-                        </a>
-                        <a href="">
-                            <div class="main-left-middle-content Collector ">
-                                <div class="main-left-middle-content-line1"></div>
-                                <img src="<?php echo IMGROOT?>/Center.png" alt="">
-                                <h2>Center Waste Management</h2>
-                            </div>
-                        </a>
-                        <a href="<?php echo URLROOT?>/centermanagers/editprofile">
-                            <div class="main-left-middle-content">
-                                <div class="main-left-middle-content-line1"></div>
-                                <img src="<?php echo IMGROOT?>/EditProfile.png" alt="">
-                                <h2>Edit Profile</h2>
-                            </div>
-                        </a>
-                    </div>
-                    <a href="<?php echo URLROOT?>/centermanagers/logout">
-                        <div class="main-left-bottom">
-                            <div class="main-left-bottom-content">
-                                <img src="<?php echo IMGROOT?>/logout.png" alt="">
-                                <p>Log out</p>
-                            </div>
-                        </div>
-                    </a>
-                </div> -->
+                <?php require APPROOT . '/views/center_managers/centermanager_sidebar/side_bar.php'; ?>
+
                 <div class="main-right">
                     <?php require APPROOT . '/views/center_managers/centermanager_requests/requests_top_bar.php'; ?>
-
-                    <!-- <div class="main-right-top">
-                        <div class="main-right-top-one">
-                            <div class="main-right-top-one-search">
-                                <img src="<?php echo IMGROOT?>/Search.png" alt="">
-                                <input id="searchInput" type="text" placeholder="Search">
-                            </div>
-
-                            <div class="main-right-top-one-content">
-                                <p><?php echo $_SESSION['center_manager_name']?></p>
-                                <img src="<?php echo IMGROOT?>/img_upload/center_manager/<?php echo $_SESSION['cm_profile']?>"
-                                    alt="">
-                            </div>
-                        </div>
-                        <div class="main-right-top-two">
-                            <h1>Requests</h1>
-                        </div>
-                        <div class="main-right-top-three">
-                            <a href="<?php echo URLROOT?>/centermanagers/request_incomming">
-                                <div class="main-right-top-three-content">
-                                    <p>Incoming</p>
-                                    <div class="line1"></div>
-                                </div>
-                            </a>
-                            <a href="<?php echo URLROOT?>/centermanagers/request_assigned">
-                                <div class="main-right-top-three-content">
-                                    <p><b style="color: #1B6652;">Assigned</b></p>
-                                    <div class="line"></div>
-                                </div>
-                            </a>
-                            <a href="<?php echo URLROOT?>/centermanagers/request_completed">
-                                <div class="main-right-top-three-content">
-                                    <p>Completed</p>
-                                    <div class="line1"></div>
-                                </div>
-                            </a>
-                            <a href="<?php echo URLROOT?>/centermanagers/request_cancelled">
-                                <div class="main-right-top-three-content">
-                                    <p>Cancelled</p>
-                                    <div class="line1"></div>
-                                </div>
-                            </a>
-                        </div>
-                        <div class="main-right-top-four">
-                            <div class="main-right-top-four-left">
-                                <p>Date</p>
-                                <input type="date" id="selected-date">
-                                <button onclick="loadLocations()">Filter</button>
-                            </div>
-                            <div class="main-right-top-four-right">
-                                <div class="main-right-top-four-component" style="background-color: #ecf0f1"
-                                    id="tables">
-                                    <img src="<?php echo IMGROOT?>/cells.png" alt="">
-                                    <p>Tables</p>
-                                </div>
-                                <div class="main-right-top-four-component" id="maps">
-                                    <img src="<?php echo IMGROOT?>/map.png" alt="">
-                                    <p>Maps</p>
-                                </div>
-
-
-                            </div>
-                        </div>
-                    </div> -->
 
                     <?php if(!empty($data['assined_requests'])) : ?>
 
@@ -170,13 +63,13 @@
                     </div>
                 </div>
                 <?php else: ?>
-                    <div class="main-right-bottom-three">
-                        <div class="main-right-bottom-three-content">
-                            <img src="<?php echo IMGROOT?>/DataNotFound.jpg" alt="">
-                            <h1>You have No Assinged Requests</h1>
-                            <p>Assigned requests will be appeared as soon as you assign collectors</p>
-                        </div>
+                <div class="main-right-bottom-three">
+                    <div class="main-right-bottom-three-content">
+                        <img src="<?php echo IMGROOT?>/DataNotFound.jpg" alt="">
+                        <h1>You have No Assinged Requests</h1>
+                        <p>Assigned requests will be appeared as soon as you assign collectors</p>
                     </div>
+                </div>
                 <?php endif; ?>
             </div>
             <div class="cancel-confirm" id="cancel-confirm">

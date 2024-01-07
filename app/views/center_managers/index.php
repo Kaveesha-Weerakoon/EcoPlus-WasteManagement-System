6a5f2b5b1ce6db200eaeb76f40185dcc3985a165
DinithiDinushika <dinithidinu689@gmail.com>
2024-01-07 18:59:37 +0530

    calendar

--- app/views/center_managers/index.php
+++ app/views/center_managers/index.php
@@ -81,6 +81,29 @@
                             </div>
 
                             <div class="right">
+                                <div class="wrapper">
+                                    <header>
+                                        <p class="current-date"></p>
+                                        <div class="icons">
+                                        <i id="prev" class='bx bx-chevron-left'></i>
+                                        <i id="next" class='bx bx-chevron-right'></i>
+                                        <!-- <span id="prev" class="material-symbols-rounded">chevron_left</span>
+                                        <span id="next" class="material-symbols-rounded">chevron_right</span> -->
+                                        </div>
+                                    </header>
+                                    <div class="calendar">
+                                        <ul class="weeks">
+                                        <li>Sun</li>
+                                        <li>Mon</li>
+                                        <li>Tue</li>
+                                        <li>Wed</li>
+                                        <li>Thu</li>
+                                        <li>Fri</li>
+                                        <li>Sat</li>
+                                        </ul>
+                                        <ul class="days"></ul>
+                                    </div>
+                                </div>
 
                             </div>
 
@@ -93,18 +116,21 @@
                                 <i class='bx bx-user'></i>
                             </div>
                             <h3>Collectors</h3>
+                            <h2>10</h2>
                         </div>
                         <div class="main-right-bottom-two-cont A" onclick="redirect_center_workers()">
                             <div class="icon_container">
                                 <i class='bx bx-group'></i>
                             </div>
                             <h3>Center Workers</h3>
+                            <h2>12</h2>
                         </div>
                         <div class="main-right-bottom-two-cont A" onclick="redirect_completed_requests()">
                             <div class="icon_container">
                                 <i class='bx bx-message-alt-check'></i>
                             </div>
                             <h3>Fulfilled Requests</h3>
+                            <h2>14</h2>
                         </div>
                         <div class="main-right-bottom-two-cont A" onclick="redirect_complaints()">
                             <div class="icon_container">
@@ -301,6 +327,63 @@ function createOrUpdateChart(color, textColor) {
 }
 
 createOrUpdateChart(color, textColor);
+
+/*Calendar*/ 
+const daysTag = document.querySelector(".days"),
+currentDate = document.querySelector(".current-date"),
+prevNextIcon = document.querySelectorAll(".icons i");
+
+// getting new date, current year and month
+let date = new Date(),
+currYear = date.getFullYear(),
+currMonth = date.getMonth();
+
+// storing full name of all months in array
+const months = ["January", "February", "March", "April", "May", "June", "July",
+              "August", "September", "October", "November", "December"];
+
+const renderCalendar = () => {
+    let firstDayofMonth = new Date(currYear, currMonth, 1).getDay(), // getting first day of month
+    lastDateofMonth = new Date(currYear, currMonth + 1, 0).getDate(), // getting last date of month
+    lastDayofMonth = new Date(currYear, currMonth, lastDateofMonth).getDay(), // getting last day of month
+    lastDateofLastMonth = new Date(currYear, currMonth, 0).getDate(); // getting last date of previous month
+    let liTag = "";
+
+    for (let i = firstDayofMonth; i > 0; i--) { // creating li of previous month last days
+        liTag += `<li class="inactive">${lastDateofLastMonth - i + 1}</li>`;
+    }
+
+    for (let i = 1; i <= lastDateofMonth; i++) { // creating li of all days of current month
+        // adding active class to li if the current day, month, and year matched
+        let isToday = i === date.getDate() && currMonth === new Date().getMonth() 
+                     && currYear === new Date().getFullYear() ? "active" : "";
+        liTag += `<li class="${isToday}">${i}</li>`;
+    }
+
+    for (let i = lastDayofMonth; i < 6; i++) { // creating li of next month first days
+        liTag += `<li class="inactive">${i - lastDayofMonth + 1}</li>`
+    }
+    currentDate.innerText = `${months[currMonth]} ${currYear}`; // passing current mon and yr as currentDate text
+    daysTag.innerHTML = liTag;
+}
+renderCalendar();
+
+prevNextIcon.forEach(icon => { // getting prev and next icons
+    icon.addEventListener("click", () => { // adding click event on both icons
+        // if clicked icon is previous icon then decrement current month by 1 else increment it by 1
+        currMonth = icon.id === "prev" ? currMonth - 1 : currMonth + 1;
+
+        if(currMonth < 0 || currMonth > 11) { // if current month is less than 0 or greater than 11
+            // creating a new date of current year & month and pass it as date value
+            date = new Date(currYear, currMonth, new Date().getDate());
+            currYear = date.getFullYear(); // updating current year with new date year
+            currMonth = date.getMonth(); // updating current month with new date month
+        } else {
+            date = new Date(); // pass the current date as date value
+        }
+        renderCalendar(); // calling renderCalendar function
+    });
+});
 </script>
 
 <?php require APPROOT . '/views/inc/footer.php'; ?>

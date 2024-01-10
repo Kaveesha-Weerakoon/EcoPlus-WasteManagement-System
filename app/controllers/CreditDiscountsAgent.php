83735035c4816de7cad48b9677b9f2721a35dfc5
Nimalsh <nimalshapg@gmail.com>
2024-01-10 14:01:34 +0530

    discount agent login

--- app/controllers/CreditDiscountsAgent.php
+++ app/controllers/CreditDiscountsAgent.php
@@ -1,6 +1,19 @@
 <?php
-  class Admin extends Controller {
+  class CreditDiscountsAgent extends Controller {
     public function __construct(){
+      $this->User_Model=$this->model('User');
+
+   
         
     }
+
+
+    public function index(){
+      $data = [
+        'title' => 'TraversyMVC',
+      ];
+     
+      $this->view('credit_discount_agents/index', $data);
+    }
+
 }

8a49f951458849a9ea6489c306398ec8618da9bf
Nimalsh <nimalshapg@gmail.com>
2024-01-24 23:43:27 +0530

    credit_discount agent logout

--- app/controllers/CreditDiscountsAgent.php
+++ app/controllers/CreditDiscountsAgent.php
@@ -16,4 +16,12 @@
       $this->view('credit_discount_agents/index', $data);
     }
 
+    public function logout(){
+      unset($_SESSION['agent_id']);
+      unset($_SESSION['agent_email']);
+      unset($_SESSION['agent_name']);
+      session_destroy();
+      redirect('users/login');
+    }
+
 }

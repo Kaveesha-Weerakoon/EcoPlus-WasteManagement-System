2afdfa115f565c7214e03fc2aecc084c838c0780
ayeshgallege <ayeshpramuditha2000@gmail.com>
2024-01-25 22:19:44 +0530

    push customer js

---
+++ public/js/Customer.js
@@ -0,0 +1,6 @@
+var profile = document.getElementsByClassName('main-right-top-profile');
+
+profile[0].addEventListener('click', function () {
+    var linkUrl = "http://localhost/ecoplus/customers/editprofile";; // Replace with your desired URL
+    window.location.href = linkUrl;
+});

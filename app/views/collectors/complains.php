0343ae3cd43aa4e1e482ce956ff48f7edbc92647
Kaveesha Weerakoon <savindukaveesha11@gmail.com>
2024-01-28 15:00:34 +0530

    push

--- app/views/collectors/complains.php
+++ app/views/collectors/complains.php
@@ -1,101 +1,118 @@
 <?php require APPROOT . '/views/inc/header.php'; ?>
 <div class="Collector_Main">
-  <div class="Collector_Complains">
-    <div class="main">
-      <?php require APPROOT . '/views/collectors/collector_sidebar/side_bar.php'; ?>
+    <div class="Collector_Complains">
+        <div class="main">
+            <?php require APPROOT . '/views/collectors/collector_sidebar/side_bar.php'; ?>
             <div class="main-right">
-              <div class="main-right-top">
-                    <div class="main-right-top-search">
+                <div class="main-right-top">
+                    <div class="main-right-top-search" style="visibility:hidden">
                         <i class='bx bxl-sketch'></i> <input type="text" placeholder="Welcome Back !" id="searchInput"
                             readonly oninput="highlightMatchingText()">
                     </div>
                     <div class="main-right-top-notification" id="notification">
                         <i class='bx bx-bell'></i>
-                        <div class="dot"></div>
+                        <?php if (!empty($data['notification'])) : ?>
+                        <div class="dot"><?php echo count($data['notification'])?></div>
+                        <?php endif; ?>
                     </div>
                     <div id="notification_popup" class="notification_popup">
                         <h1>Notifications</h1>
-                        <div class="notification">
-                            <div class="notification-green-dot">
+                        <div class="notification_cont">
+                            <?php foreach($data['notification'] as $notification) : ?>
 
+                            <div class="notification">
+                                <div class="notification-green-dot">
+
+                                </div>
+                                <div class="notification_right">
+                                    <p><?php echo date('Y-m-d', strtotime($notification->datetime)); ?></p>
+                                    <?php echo $notification->notification ?>
+                                </div>
                             </div>
-                            Request 1232 Has been Cancelled
-                        </div>
-                        <div class="notification">
-                            <div class="notification-green-dot">
+                            <?php endforeach; ?>
 
-                            </div>
-                            Request 1232 Has been Assigned
                         </div>
-                        <div class="notification">
-                            <div class="notification-green-dot">
-
-                            </div>
-                            Request 1232 Has been Cancelled
-                        </div>
-
+                        <form class="mark_as_read" method="post" action="<?php echo URLROOT;?>/collectors/">
+                            <i class="fa-solid fa-check"> </i>
+                            <button type="submit">Mark all as read</button>
+                        </form>
 
                     </div>
                     <div class="main-right-top-profile">
                         <img src="<?php echo IMGROOT?>/img_upload/collector/<?php echo $_SESSION['collector_profile']?>"
                             alt="">
                         <div class="main-right-top-profile-cont">
-                        <h3><?php echo $_SESSION['collector_name']?></h3>
+                            <h3><?php echo $_SESSION['collector_name']?></h3>
                             <p>ID : C <?php echo $_SESSION['collector_id']?></p>
                         </div>
                     </div>
                 </div>
                 <div class="main-bottom">
-                    <div class="main-bottom-component">
-                        <form class="main-bottom-component-left" action="<?php echo URLROOT;?>/collectors/complains"
-                            method="post">
-                            <div class="main-bottom-component-left-topic">
-                                <h2>Make a Complain</h2>
-                                <div class="line"></div>
-                            </div>
-
-                            <div class="main-bottom-component-left-component">
-                                <h2>Name</h2>
-                                <input value="<?php echo $data['name']; ?>" type="text" name="name" placeholder="Name">
-                                <div class="err"><?php echo $data['name_err']; ?></div>
-                            </div>
-
-                            <div class="main-bottom-component-left-component">
-                                <h2>Contact Number</h2>
-                                <input value="<?php echo $data['contact_no']; ?>" name="contact_no" type="text"
-                                    placeholder="Contact Number">
-                                <div class="err"><?php echo $data['contact_no_err']; ?></div>
-                            </div>
-
-                            <div class="main-bottom-component-left-component">
-                                <h2>Region</h2>
-                                <input type="text" value="<?php echo $data['region']; ?>" name="region"
-                                    placeholder="Region">
-                                <div class="err"><?php echo $data['region_err']; ?></div>
-                            </div>
-
-                            <div class="main-bottom-component-left-component">
-                                <h2>Subject</h2>
-                                <input value="<?php echo $data['subject']; ?>" name="subject" type="text"
-                                    placeholder="Subject">
-                                <div class="err"><?php echo $data['subject_err']; ?></div>
-                            </div>
-
-                            <div class="main-bottom-component-left-component">
-                                <h2>Complain</h2>
-                                <input value="<?php echo $data['complain']; ?>" name="complain" type="text"
-                                    placeholder="Complain">
-                                <div class="err"><?php echo $data['complain_err']; ?></div>
-                            </div>
-                            <div class="main-bottom-component-left-button">
-                                <button type="submit">Make Complain</button>
-                            </div>
-                        </form>
-                        <div class="main-bottom-component-right">
-                            <img src="<?php echo IMGROOT?>/makeComplaints.png" alt="" />
+                    <div class="main-bottom-top">
+                        <h2>Complaints</h2>
+                        <div class="main-bottom-top-cont">
+                            <a href="">
+                                <div class="main-bottom-top-content">
+                                    <p><b style="color:#1ca557;">Complaint</b></p>
+                                    <div class="line2"></div>
+                                </div>
+                            </a>
+                            <a href="<?php echo URLROOT?>/collectors/complains_history">
+                                <div class="main-bottom-top-content">
+                                    <p>History</p>
+                                    <div class="line1"></div>
+                                </div>
+                            </a>
                         </div>
-
                     </div>
+                    <div class="main-bottom-down">
+                        <div class="main-bottom-component">
+                            <form class="main-bottom-component-left" action="<?php echo URLROOT;?>/collectors/complains"
+                                method="post">
+                                <div class="main-bottom-component-left-topic">
+                                    <h2>Make a Complaint</h2>
+                                    <div class="line"></div>
+                                </div>
+
+                                <div class="main-bottom-component-left-component">
+                                    <h2>Name</h2>
+                                    <input value="<?php echo $data['name']; ?>" type="text" name="name"
+                                        placeholder="Name">
+                                    <div class="err"><?php echo $data['name_err']; ?></div>
+                                </div>
+
+                                <div class="main-bottom-component-left-component">
+                                    <h2>Contact Number</h2>
+                                    <input value="<?php echo $data['contact_no']; ?>" name="contact_no" type="text"
+                                        placeholder="Contact Number">
+                                    <div class="err"><?php echo $data['contact_no_err']; ?></div>
+                                </div>
+
+                                <div class="main-bottom-component-left-component">
+                                    <h2>Subject</h2>
+                                    <input value="<?php echo $data['subject']; ?>" name="subject" type="text"
+                                        placeholder="Subject">
+                                    <div class="err"><?php echo $data['subject_err']; ?></div>
+                                </div>
+
+                                <div class="main-bottom-component-left-component">
+                                    <h2>Complain</h2>
+                                    <input value="<?php echo $data['complain']; ?>" name="complain" type="text"
+                                        placeholder="Complain">
+                                    <div class="err"><?php echo $data['complain_err']; ?></div>
+                                </div>
+                                <div class="main-bottom-component-left-button">
+                                    <button type="submit">Submit</button>
+                                </div>
+                            </form>
+                            <!-- <div class="main-bottom-component-right">
+                                <img src="<?php echo IMGROOT?>/makeComplaints.png" alt="" />
+                            </div> -->
+
+                        </div>
+                    </div>
+
+
                 </div>
                 <?php if($data['completed']=='True') : ?>
                 <div class="complain_success" style="">
@@ -103,7 +120,8 @@
                         <img src="<?php echo IMGROOT?>/check.png" alt="">
                         <h2>Success!!</h2>
                         <p>Complain has been reported successfully</p>
-                        <a href="<?php echo URLROOT?>/collectors/history_complains"><button type="button">OK</button></a>
+                        <a href="<?php echo URLROOT?>/collectors/history_complains"><button
+                                type="button">OK</button></a>
 
 
                     </div>
@@ -112,8 +130,8 @@
             </div>
 
         </div>
-  </div>
-    
+    </div>
+
 </div>
 
 <?php require APPROOT . '/views/inc/footer.php'; ?>
